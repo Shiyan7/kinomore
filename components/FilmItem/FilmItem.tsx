7ac86cf964dfd6fea58f346290bf696eb604291f
Shiyan7 <57950265+Shiyan7@users.noreply.github.com>
2022-05-29 21:53:18 +0300

    some fixes

--- components/FilmItem/FilmItem.tsx
+++ components/FilmItem/FilmItem.tsx
@@ -14,7 +14,7 @@ export const FilmItem: FC<FilmItemProps> = ({item}) => {
 
     return (
         <li className={styles.item}>
-            <div className={styles.poster}>
+            <div className={styles.top}>
                 <Link href='#'>
                     <a className={styles.imageContainer}>
                         <Image
@@ -32,7 +32,7 @@ export const FilmItem: FC<FilmItemProps> = ({item}) => {
                 <button className={classNames('btn-reset', styles.favorite)}><FiBookmark /></button>
             </div>
             <Link href='#'>
-                <a className={styles.title}>{item.name}</a>
+                <a className={styles.title}>{item.names[0].name}</a>
             </Link>
             <span className={styles.info}>{item.year}, {item.type === 'movie' ? 'фильм' : 'сериал'}</span>
         </li>
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,11 +1,10 @@
 import styles from './Header.module.scss'
-import {FiFilm, FiMenu, FiHome, FiUser, FiTv, FiHeart, FiSearch} from 'react-icons/fi'
+import {FiFilm, FiMenu, FiHome, FiUser, FiTv, FiHeart} from 'react-icons/fi'
 import classNames from 'classnames'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { Search } from '../Search/Search'
 import { useRef, useState } from 'react'
-import OutsideClickHandler from 'react-outside-click-handler';
 import { useOnClickOutside } from 'usehooks-ts'
 
 export const Header = () => {
@@ -23,14 +22,13 @@ export const Header = () => {
         {icon: <FiHome />, href: '/', text: 'Главная'},
         {icon: <FiFilm />, href: '/films', text: 'Фильмы'},
         {icon: <FiTv />, href: '/series', text: 'Сериалы'},
-        {icon: <FiHeart />, href: '/favorites', text: 'Избранное'},
-        {icon: <FiUser />, href: '/auth', text: 'Войти'}
+        {icon: <FiHeart />, href: '/favorites', text: 'Избранное'}
     ]
 
     return (
-        <header className={styles.header}>
+        <header ref={ref} className={styles.header}>
             <div className={classNames('container', styles.container)}>
-                <div ref={ref} className={styles.top}>
+                <div ref={ref} className={styles.menu}>
                     <button
                         className={classNames('btn-reset', styles.burger)}
                         onClick={handleOpen}
