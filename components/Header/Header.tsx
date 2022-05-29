1a14f971bdddbf77ba4168bc06197f141118a5ec
Shiyan7 <57950265+Shiyan7@users.noreply.github.com>
2022-05-29 19:36:58 +0300

    pwa

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -4,6 +4,8 @@ import classNames from 'classnames'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { Search } from '../Search/Search'
+import { useState } from 'react'
+import OutsideClickHandler from 'react-outside-click-handler';
 
 export const Header = () => {
 
@@ -16,30 +18,39 @@ export const Header = () => {
     ]
 
     const router = useRouter()
+    const [open, setOpen] = useState(false);
+
+    const handleOpen = () => setOpen(!open)
 
     return (
         <header className={styles.header}>
             <div className={classNames('container', styles.container)}>
-                <div className={styles.top}>
-                    <div className={styles.burger}>
-                        <FiMenu />
+                
+                <OutsideClickHandler onOutsideClick={handleOpen}>
+                    <div className={styles.top}>
+                        <button
+                            className={classNames('btn-reset', styles.burger)}
+                            onClick={handleOpen}
+                        >
+                            <FiMenu />
+                        </button>
+                        <span className={styles.logo}>Kinomore</span>
+                        <div className={classNames(styles.dropdown, open && styles.dropdownOpen)}>
+                            <ul className={classNames('list-reset', styles.dropdownList)}>
+                                {items.map(el => (
+                                    <li key={el.text} className={styles.dropdownItem}>
+                                        <Link href={el.href}>
+                                            <a className={classNames(styles.dropdownLink, router.pathname === el.href && styles.dropdownLinkActive)}>
+                                                {el.icon}
+                                                {el.text}
+                                            </a>
+                                        </Link>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
                     </div>
-                    <span className={styles.logo}>Kinomore</span>
-                    <div className={styles.dropdown}>
-                        <ul className={classNames('list-reset', styles.dropdownList)}>
-                            {items.map(el => (
-                                <li key={el.text} className={styles.dropdownItem}>
-                                    <Link href={el.href}>
-                                        <a className={classNames(styles.dropdownLink, router.pathname === el.href && styles.dropdownLinkActive)}>
-                                            {el.icon}
-                                            {el.text}
-                                        </a>
-                                    </Link>
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-                </div>
+                </OutsideClickHandler>
                 <Search />
                 <Link href="/auth">
                     <a className={styles.link}>Войти</a>
--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
-module.exports = {
-  reactStrictMode: true,
-  images: {
-    domains: ['st.kp.yandex.net'],
-  }
-}
+const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache')
+const withPlugins = require('next-compose-plugins');
+
+const nextConfig = {
+    images: {
+        domains: ['st.kp.yandex.net'],
+    }
+};
+
+module.exports = withPlugins([
+    [withPWA, {
+        pwa: {
+            dest: 'public',
+            runtimeCaching
+        }
+    }]
+], nextConfig)
