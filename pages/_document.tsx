d8d327c51e91331dd44c406d2ce913a0d2b28cae
Shiyan7 <57950265+Shiyan7@users.noreply.github.com>
2022-05-29 20:01:35 +0300

    fix pwa

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,7 +11,7 @@ export default function Document() {
         <link href="/icons/favicon-16x16.png" rel="icon" type="image/png" sizes="16x16" />
         <link href="/icons/favicon-32x32.png" rel="icon" type="image/png" sizes="32x32" />
         <link rel="apple-touch-icon" href="/apple-icon.png"></link>
-        <meta name="theme-color" content="#1e213a" />
+        <meta name="theme-color" content="#1f1f1f" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:wght@300;400;500;700&family=Oswald:wght@300;400&display=swap" rel="stylesheet" />
