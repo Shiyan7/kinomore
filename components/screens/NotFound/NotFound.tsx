892995aaf820064c5354f7b18f7ad86808513579
Shiyan7 <57950265+Shiyan7@users.noreply.github.com>
2022-06-01 15:53:51 +0300

    some fixes

--- components/screens/NotFound/NotFound.tsx
+++ components/screens/NotFound/NotFound.tsx
@@ -1,11 +1,10 @@
-import classNames from 'classnames'
 import Link from 'next/link'
-import React from 'react'
+import classNames from 'classnames'
 import styles from './NotFound.module.scss'
 
 export const NotFound = () => {
   return (
-    <section className="not-found">
+    <section className={styles.section}>
         <div className={classNames('container', styles.container)}>
             <h1 className={classNames('g-title', styles.title)}>404. Страница не найдена</h1>
             <p className={styles.desc}>Возможно, она была перемещена, или вы просто неверно указали адрес страницы.</p>
