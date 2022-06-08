b7ba5fb326a314e1247abbb7906bcde2630a8c0d
Shiyan7 <57950265+Shiyan7@users.noreply.github.com>
2022-06-09 00:38:58 +0300

    some fixes

--- components/screens/Films/Films.tsx
+++ components/screens/Films/Films.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link"
-import { Textfit } from 'react-textfit';
 import { BEST_FILMS_ROUTE, WAR_FILMS_ROUTE } from "../../../constants/routes";
 
 export const Films = () => {
@@ -8,10 +7,7 @@ export const Films = () => {
       <li className="movie-list__item">
         <Link href={BEST_FILMS_ROUTE}>
           <a className="movie-item">  
-            <Textfit
-              className="movie-item__poster"
-              mode='single'
-            >Фильмы</Textfit>
+            <span className="movie-item__poster">Лучшие</span>
             <div className="movie-item__text">
               <h2 className="movie-item__title">250 лучших фильмов</h2>
               <p className="movie-item__desc">250 фильмов</p>
@@ -22,10 +18,7 @@ export const Films = () => {
       <li className="movie-list__item">
         <Link href='#'>
           <a className="movie-item">
-            <Textfit
-              className="movie-item__poster"
-              mode='single'
-            >Комедии</Textfit>
+            <span className="movie-item__poster">Комедии</span>
             <div className="movie-item__text">
               <h2 className="movie-item__title">250 лучших коммедий</h2>
               <p className="movie-item__desc">250 коммедий</p>
@@ -36,27 +29,10 @@ export const Films = () => {
       <li className="movie-list__item">
         <Link href={WAR_FILMS_ROUTE}>
           <a className="movie-item">
-            <Textfit
-              className="movie-item__poster"
-              mode='single'
-            >Военные</Textfit>
+            <span className="movie-item__poster">Военные</span>
             <div className="movie-item__text">
               <h2 className="movie-item__title">250 лучших военных фильмов</h2>
               <p className="movie-item__desc">250 военных фильмов</p>
-            </div>
-          </a>
-        </Link>
-      </li>
-      <li className="movie-list__item">
-        <Link href='#'>
-          <a className="movie-item">
-            <Textfit
-              className="movie-item__poster"
-              mode='single'
-            >Документальные</Textfit>
-            <div className="movie-item__text">
-              <h2 className="movie-item__title">250 лучших документальных фильмов</h2>
-              <p className="movie-item__desc">250 документальных фильмов</p>
             </div>
           </a>
         </Link>
