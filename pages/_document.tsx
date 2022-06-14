e7d16f4e468b78f9aa64d37e8d4acf91c3697bff
Shiyan7 <57950265+Shiyan7@users.noreply.github.com>
2022-06-14 21:04:44 +0300

    fix fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,11 +11,6 @@ export default function Document() {
         <link href="/icons/favicon-32x32.png" rel="icon" type="image/png" sizes="32x32" />
         <link rel="apple-touch-icon" href="/apple-icon.png"></link>
         <meta name="theme-color" content="#1f1f1f" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Fira+Sans:wght@300;400;500;600;700;800&display=swap"
-          rel="stylesheet"
-        />
       </Head>
       <body>
         <Main />
