86eb0ae0f9cbf8bfc533ec2c37de6becdb54788f
Shiyan7 <57950265+Shiyan7@users.noreply.github.com>
2022-07-01 22:26:31 +0300

    some fixes

--- components/Filters/Filters.tsx
+++ components/Filters/Filters.tsx
@@ -1,15 +1,17 @@
-import {useEffect} from 'react';
-import {useSwipeable} from 'react-swipeable';
-import {useTypedSelector} from '@/hooks/useTypedSelector';
-import {useActions} from '@/hooks/useActions';
+import {useEffect} from "react";
+import {useTypedSelector} from "@/hooks/useTypedSelector";
+import {useActions} from "@/hooks/useActions";
 import {Filter} from "./components/Filter/Filter"
 import {Slider} from "@/components/Slider/Slider";
 import {Radio} from "@/components/Radio/Radio";
 import {Button} from "@/components/Button/Button";
-import {Select} from '@/components/Select/Select';
-import {getCurrentYear} from '@/helpers/getCurrentYear/getCurrentYear';
+import {Select} from "@/components/Select/Select";
+import {getCurrentYear} from "@/helpers/getCurrentYear/getCurrentYear";
 import {Controller, useForm} from 'react-hook-form';
-import styles from './Filters.module.scss';
+import {Title} from "../Title/Title";
+import {ButtonBase} from "../ButtonBase/ButtonBase";
+import {FiX} from "react-icons/fi";
+import styles from "./Filters.module.scss";
 import classNames from "classnames";
 
 export const Filters = () => {
@@ -24,19 +26,12 @@ export const Filters = () => {
       setSwipedValue,
       resetFilters,
     } = useActions();
-    const {swipedValue, genres} = useTypedSelector(state => state.filtersReducer)
+    const {genres} = useTypedSelector(state => state.filtersReducer);
     const {openedFilters} = useTypedSelector(state => state.toggleReducer);
 
     const handleClose = () => {
         toggleFilters(false)
     }
-
-    const handlers = useSwipeable({
-        onSwiping: e => setSwipedValue(e.deltaY),
-        onSwipedUp: () => setSwipedValue(0),
-        onSwipedDown: () => handleClose(),
-        trackMouse: false,
-    });
 
     const {handleSubmit, control, register, reset} = useForm({
         defaultValues: {
@@ -79,12 +74,25 @@ export const Filters = () => {
             action="#"
             onSubmit={onSubmit}
             noValidate
-            onClick={handleClose}
             className={classNames(styles.filters, openedFilters && styles.opened)}
         >
-            <div style={{transform: `translateY(${swipedValue}px)`}} onClick={e => e.stopPropagation()} className={styles.content}>
-                <div {...handlers} onClick={handleClose} className={styles.top}></div>
-                <div className={styles.container}>
+                <div className={styles.top}>
+                    <Button
+                        type="button"
+                        onClick={handleReset}
+                        variant="sm"
+                    >
+                        Сбросить
+                    </Button>
+                    <Title variant='h3' className={styles.title}>Фильтры</Title>
+                    <ButtonBase
+                        className={styles.close}
+                        onClick={handleClose}
+                    >
+                        <FiX />
+                    </ButtonBase>
+                </div>
+                <div className={styles.content}>
                     <Filter name="Рейтинг">
                         <Controller
                             name="rating"
@@ -150,12 +158,11 @@ export const Filters = () => {
                             />
                         </div>
                     </Filter>
-                    <div className={styles.btns}>
-                        <Button className={styles.btn}>Применить</Button>
-                        <Button type='button' className={styles.btn} onClick={handleReset} variant='stroke'>Сбросить</Button>
-                    </div>
                 </div>
-            </div>
+                <div className={styles.btns}>
+                    <Button className={styles.btn}>Применить</Button>
+                    <Button type="button" className={classNames(styles.btn, styles.reset)} onClick={handleReset} variant="stroke">Сбросить</Button>
+                </div>
         </form>
     )
 }
--- store/actions.ts
+++ store/actions.ts
@@ -1,5 +1,5 @@
 import { loadMoreFilms, loadMoreSeries } from "@/store/reducers/loadMore.slice";
-import { setFilterRatings, setFiterYears, setSortByRelease, setFilterGenre, resetFilters, setSwipedValue } from "@/store/reducers/filters.slice";
+import { setFilterRatings, setFiterYears, setSortByRelease, setFilterGenre, resetFilters } from "@/store/reducers/filters.slice";
 import { toggleFilters, toggleMenu } from "@/store/reducers/toggle.slice";
 import { setPage } from "@/store/reducers/pagination.slice";
 import { setSearch } from "@/store/reducers/search.slice";
@@ -12,7 +12,6 @@ export {
     setFilterRatings,
     setFiterYears,
     setSortByRelease,
-    setSwipedValue,
     setFilterGenre,
     setPage,
     resetFilters,
