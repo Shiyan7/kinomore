cc0b124e7573bf0800a411f17308098e2cf92df2
Shiyan7 <57950265+Shiyan7@users.noreply.github.com>
2022-07-12 11:50:16 +0300

    fix hook favourites

--- src/hooks/useFavourite.ts
+++ src/hooks/useFavourite.ts
@@ -1,17 +1,16 @@
-import {useCallback} from "react";
 import {useLocalStorage} from "usehooks-ts";
 
 export const useFavourites = () => {
   const [favourites, setFavourites] = useLocalStorage<number[]>('favourites', []);
 
-  const toggleFavourite = useCallback((id: number) => {
+  const toggleFavourite = (id: number) => {
     if (favourites.includes(id)) {
       setFavourites(favourites.filter((favourite: number) => favourite !== id));
     } else {
       setFavourites([...favourites, id]);
     }
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  };
 
   return {
     favourites,
