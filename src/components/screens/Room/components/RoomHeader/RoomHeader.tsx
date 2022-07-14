99abfb80de8021cab5df0366187231b785e2edb9
Ivan Admaers <admaers@icloud.com>
2022-07-14 12:18:06 +0400

    prettify

--- src/components/screens/Room/components/RoomHeader/RoomHeader.tsx
+++ src/components/screens/Room/components/RoomHeader/RoomHeader.tsx
@@ -1,18 +1,18 @@
-import {FC} from 'react'
-import {BackBtn} from '@/UI/BackBtn/BackBtn';
-import styles from './RoomHeader.module.scss'
+import { FC } from 'react';
+import { BackBtn } from '@/UI/BackBtn/BackBtn';
+import styles from './RoomHeader.module.scss';
 
 interface RoomHeaderProps {
-    title: string | undefined;
+  title: string | undefined;
 }
 
-export const RoomHeader: FC<RoomHeaderProps> = ({title}) => {
-    return (
-        <header className={styles.header}>
-            <div className={styles.container}>
-                <BackBtn className={styles.btn} variant='icon' />
-                <h1 className={styles.title}>{title}</h1>
-            </div>
-        </header>
-    )
-}
+export const RoomHeader: FC<RoomHeaderProps> = ({ title }) => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <BackBtn className={styles.btn} variant="icon" />
+        <h1 className={styles.title}>{title}</h1>
+      </div>
+    </header>
+  );
+};
