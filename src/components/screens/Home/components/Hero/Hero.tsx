862e7f5c206b0754a89ac46256da32df97c3c1ce
Jeka Shiyan <ncringe31@gmail.com>
2022-07-20 16:11:44 +0300

    some fixes

--- src/components/screens/Home/components/Hero/Hero.tsx
+++ src/components/screens/Home/components/Hero/Hero.tsx
@@ -13,39 +13,19 @@ import 'swiper/css/effect-fade';
 export const Hero = () => {
 
 	const {push} = useRouter()
-	const items = [
-		{title: 'Анчартед: На картах не значится', desc: 'Нейтан Дрейк и Виктор «Салли» Салливан, два искателя приключений, отправляются на поиски величайшего сокровища мира.', id: 468373, imageSource: '/bg1.jpg'},
-		{title: 'Тор: Любовь и гром', desc: 'Джейн Фостер берет на себя обязанности Бога-громовержца и становится обладательницей молота Мьёльнира.', id: 1282688, imageSource: '/bg2.jpg'}
-	]
 
 	return (
-		<section className={styles.section}>
+		<section className={styles.section} style={{backgroundImage: 'url(/bg1.jpg)'}}>
 			<h1 className='visually-hidden'>Kinomore — бесплатные фильмы и сериалы</h1>
-			<Swiper
-				modules={[Autoplay, EffectFade]}
-				autoplay
-				effect='fade'
-			>
-				{items.map(item => {
-
-					const {title, desc, id, imageSource} = item
-
-					return (
-						<SwiperSlide key={id}>
-							<div className={styles.content}>
-								<Image className={styles.image} quality='100%' src={imageSource} alt={title} layout='fill'  />
-								<div className={classNames('container', styles.container)}>
-									<Title variant='h2' className={styles.title}>{title}</Title>
-									<p className={styles.desc}>{desc}</p>
-									<Button onClick={() => push(`/film/${id}`)} endIcon={<FiArrowRight />}>
-										Подробнее
-									</Button>
-								</div>
-							</div>
-						</SwiperSlide>
-					)
-					})}
-			</Swiper>
+			<div className={classNames('container', styles.container)}>
+				<div className={styles.content}>
+					<Title variant='h2' className={styles.title}>Тор: Любовь и гром</Title>
+					<p className={styles.desc}>Джейн Фостер берет на себя обязанности Бога-громовержца и становится обладательницей молота Мьёльнира.</p>
+					<Button onClick={() => push(`/film/1282688`)} endIcon={<FiArrowRight />}>
+						Подробнее
+					</Button>
+				</div>
+			</div>
 		</section>
 	)
 }
