b519b42a408cbbefdf67f95bd6d48ccc75d0ef0c
Jeka Shiyan <ncringe31@gmail.com>
2022-10-03 00:42:08 +0300

    some fixes

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,7 +9,7 @@ export default function Document() {
 				<link href="/icons/favicon-16x16.png" rel="icon" type="image/png" sizes="16x16" />
 				<link href="/icons/favicon-32x32.png" rel="icon" type="image/png" sizes="32x32" />
 				<link rel="apple-touch-icon" href="/apple-icon.png"></link>
-				<meta name="theme-color" content="#1f1f1f" />
+				<meta name="theme-color" content="#141414" />
 			</Head>
 			<body>
 				<Main />
